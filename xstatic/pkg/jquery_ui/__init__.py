b0fbfaf4b4053672508dcd4ba1b96655a2a315f2
Thomas Waldmann <tw AT waldmann-edv DOT de>
2014-08-09 21:51:40 +0200

    fix pypi package name

--- xstatic/pkg/jquery_ui/__init__.py
+++ xstatic/pkg/jquery_ui/__init__.py
@@ -4,7 +4,7 @@ XStatic resource package
 See package 'XStatic' for documentation and basic tools.
 """
 
-DISPLAY_NAME = 'jquery_ui' # official name, upper/lowercase allowed, no spaces
+DISPLAY_NAME = 'jquery-ui' # official name, upper/lowercase allowed, no spaces
 PACKAGE_NAME = 'XStatic-%s' % DISPLAY_NAME # name used for PyPi
 
 NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
